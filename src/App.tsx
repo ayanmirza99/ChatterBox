38f2f8b79575d3882fb4be5ad61935a4717133c4
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2024-07-12 18:40:32 +0500

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
-const App = () => {
+export function getUrlParams(url = window.location.href) {
+  let urlStr = url.split("?")[1];
+  return new URLSearchParams(urlStr);
+}
+function App() {
+  function randomID(len: number) {
+    let result = "";
+    if (result) return result;
+    var chars =
+        "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+  const roomID = getUrlParams().get("roomID") || randomID(5);
+  let myMeeting = async (element: any) => {
+    // generate Kit Token
+    const appID = parseInt(import.meta.env.VITE_APP_ID);
+    const serverSecret = import.meta.env.VITE_SERVER_SECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      randomID(5),
+      randomID(5)
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Personal link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+
   return (
-    <div className='h-screen w-screen bg-red-700'>App</div>
-  )
+    <>
+      <div className="h-[8vh] w-full flex items-center justify-center bg-[#1c1f2e]">
+        <h1 className="text-white font-bold text-5xl">
+          <span className="text-[#2c72ff]">C</span>hatter
+          <span className="text-[#2c72ff]">B</span>ox
+        </h1>
+      </div>
+      <div
+        className="myCallContainer"
+        ref={myMeeting}
+        style={{ width: "100%", height: "92vh" }}
+      ></div>
+    </>
+  );
 }
 
-export default App
+export default App;
